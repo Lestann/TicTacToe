f6deec08557f171c0f20eff4cf59d0579a53a04d
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-10-16 22:09:34 +0000

    demo test to test Travis

---
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -0,0 +1,12 @@
+package is.ru.tictactoe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TicTacToeTest {
+
+  @Test
+  public void demoTestShouldAlwaysPasss() {
+    assertEquals(1, 1);
+  }
+}
