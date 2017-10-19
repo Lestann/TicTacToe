868d23e46847d7a864c5093c44cd2173e723ea85
kolliarnorsson <kolbeinna16@ru.is>
2017-10-19 17:05:08 +0000

    check for winner horizontal

--- src/main/java/is/tictactoe/Board.java
+++ src/main/java/is/tictactoe/Board.java
@@ -5,7 +5,7 @@ public class Board
 {
 
 	char[][] board;
-	private final int constSize = 3; 
+	private final int constSize = 3;
 
 	public Board() {
 		board = new char[constSize][constSize];
@@ -20,30 +20,23 @@ public class Board
 	}
 
 	public void print() {
-		
+
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				System.out.print(board[i][j] + " ");	
+				System.out.print(board[i][j] + " ");
 			}
 			System.out.println();
 		}
-	} 
+	}
 
 	public void changeBoard(Symbol symbol, int position)
 	{
 		int count = 1;
-		char player = 'X';
+		char player = getSymbol(symbol);
 
-		if (symbol == Symbol.CIRCLE) {
-			player = 'O';
-		}
-		else {
-			player = 'X';
-		}
-
-		for(int i = 0; i < 3; i++)
+		for(int i = 0; i < constSize; i++)
 		{
-			for(int j = 0; i < 3; i++)
+			for(int j = 0; j < constSize; j++)
 			{
 				if(position == count)
 				{
@@ -53,6 +46,30 @@ public class Board
 				count++;
 			}
 		}
+	}
 
+	public boolean checkWinner(Player p)
+	{
+		int counter = 0;
+		char player = getSymbol(p.getSymbol());
+		for(int i = 0; i < constSize; i++)
+		{
+			for(int j = 0; j < constSize; j++)
+			{
+				if (board[i][j] == player){ counter++; }
+				else { counter = 0; }
+				if(counter == constSize) return true;
+			}
+		}
+		return false;
+	}
+
+	private char getSymbol(Symbol symbol){
+		if (symbol == Symbol.CIRCLE) {
+			return 'O';
+		}
+		else {
+			return 'X';
+		}
 	}
 }
--- src/test/java/is/ru/tictactoe/BoardTest.java
+++ src/test/java/is/ru/tictactoe/BoardTest.java
@@ -4,5 +4,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest{
-
+  @Test
+  public void testCheckWinner()
+  {
+    Player player = new Player("Joi", Symbol.CROSS);
+    Board board = new Board();
+    board.changeBoard(player.getSymbol(), 1);
+    board.changeBoard(player.getSymbol(), 2);
+    board.changeBoard(player.getSymbol(), 3);
+    assertEquals(true, board.checkWinner(player));
+  }
 }
