5c8109b47c460fbc0fe6fad62623beac76159c51
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-10-19 15:38:51 +0000

    Initilize TicTacToe

--- src/main/java/is/tictactoe/TicTacToe.java
+++ src/main/java/is/tictactoe/TicTacToe.java
@@ -1,5 +1,13 @@
 package is.ru.tictactoe;
 
 public class TicTacToe {
-  // sfsadfsdf
+
+  private Board board;
+  private Player player1, player2;
+
+  public TicTacToe (Player player1, Player player2) {
+    this.board = new Board();
+    this.player1 = player1;
+    this.player2 = player2;
+  }
 }
