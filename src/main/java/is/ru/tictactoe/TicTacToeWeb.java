2bcb9a801dd0dd748bc8b658019598613e71259c
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-10-24 18:05:42 +0000

    renaming web file

--- src/main/java/is/ru/tictactoe/TicTacToeWeb.java
+++ src/main/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -4,10 +4,10 @@ import spark.*;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 
-public class TicWeb implements SparkApplication {
+public class TicTacToeWeb implements SparkApplication {
   public static void main(String[] args) {
     staticFileLocation("/public");
-    SparkApplication tictactoe = new TicWeb();
+    SparkApplication tictactoe = new TicTacToeWeb();
     port(getHerokuPort());
 
     post("/newGame", (req, res) -> {
