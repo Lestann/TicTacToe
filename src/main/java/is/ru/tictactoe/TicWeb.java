e07b35b644916bcf225efec5d9659e1fa65bdfab
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-10-24 17:14:42 +0000

    fix heroku

--- src/main/java/is/ru/tictactoe/TicWeb.java
+++ src/main/java/is/ru/tictactoe/TicWeb.java
@@ -6,9 +6,19 @@ import spark.servlet.SparkApplication;
 
 public class TicWeb implements SparkApplication {
   public static void main(String[] args) {
+    port(getHerokuPort());
+
     get("/", (req, res) -> {
       return "Hello World!";
     });
+  }
+
+  static int getHerokuPort() {
+    ProcessBuilder psb = new ProcessBuilder();
+    if (psb.environment().get("PORT") != null) {
+      return Integer.parseInt(psb.environment().get("PORT"));
+    }
+    return 4567;
   }
 
   @Override
