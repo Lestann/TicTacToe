b8f3d97c042c429e00a2751a3d312938c9b7091f
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-10-24 17:07:19 +0000

    Heroku setup #1

--- src/main/java/is/ru/tictactoe/TicWeb.java
+++ src/main/java/is/ru/tictactoe/TicWeb.java
@@ -1,11 +1,18 @@
 package is.ru.tictactoe;
 
+import spark.*;
 import static spark.Spark.*;
+import spark.servlet.SparkApplication;
 
-public class TicWeb {
+public class TicWeb implements SparkApplication {
   public static void main(String[] args) {
     get("/", (req, res) -> {
-      return "HELLO WORLD!";
+      return "Hello World!";
     });
+  }
+
+  @Override
+  public void init() {
+    //
   }
 }
