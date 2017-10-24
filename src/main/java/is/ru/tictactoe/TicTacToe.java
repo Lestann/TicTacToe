bc265e236994d5cf6109690c834c79dac4de04ed
kolliarnorsson <kolbeinna16@ru.is>
2017-10-24 12:31:54 +0000

    Works for one player to play

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -13,28 +13,37 @@ public class TicTacToe {
     this.player2 = player2;
   }
 
-  public void play() {
+  public void play(Scanner scanner) {
     boolean continueGame = true;
     Player player = player1;
     while (continueGame) {
       for (int i = 0; i < SIZE*SIZE; i++) {
+        System.out.println();
+        board.print();
+        System.out.println();
+        System.out.print(player.getName() + ", its your move: ");
+        int input = scanner.nextInt();
+        board.changeBoard(player.getSymbol(), input);
         if (board.checkWinner(player)){
           break;
-        }
+        }     
       }
     }
   }
 
   public static void main(String[] args){
       Scanner scanner = new Scanner(System.in);
+      System.out.println();
       System.out.print("Please enter a name for player1: ");
       String p1Name = getName(scanner);
       Player player1 = new Player(p1Name, Symbol.CROSS);
+      System.out.println();
       System.out.print("Please enter a name for player2: ");
       String p2Name = getName(scanner);
-      scanner.close();
       Player player2 = new Player(p1Name, Symbol.CROSS);
       TicTacToe game = new TicTacToe(player1, player2);
+      game.play(scanner);
+      scanner.close();
   }
 
   private static String getName(Scanner scanner){
