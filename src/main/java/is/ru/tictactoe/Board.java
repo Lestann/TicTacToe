90092c57811b5b6a44b309cfd1539ca74588a25e
Vigdís Birna Þorsteinsdóttir <vigdis16@ru.is>
2017-10-25 10:55:39 +0000

    Fixing code

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -3,7 +3,6 @@ package is.ru.tictactoe;
 
 public class Board
 {
-
 	char[][] board;
 	private final int constSize = 3;
 
@@ -29,17 +28,13 @@ public class Board
 		}
 	}
 
-	public void changeBoard(Symbol symbol, int position)
-	{
+	public void changeBoard(Symbol symbol, int position) {
 		int count = 1;
 		char player = getSymbol(symbol);
 
-		for(int i = 0; i < constSize; i++)
-		{
-			for(int j = 0; j < constSize; j++)
-			{
-				if(position == count)
-				{
+		for (int i = 0; i < constSize; i++) {
+			for (int j = 0; j < constSize; j++) {
+				if (position == count) {
 					board[i][j] = player;
 					return;
 				}
@@ -48,18 +43,17 @@ public class Board
 		}
 	}
 
-	public boolean checkWinner(Player p)
-	{
+	public boolean checkWinner(Player p) {
 		int horizontal = 0;
 		int vertical = 0;
 		int diagnallr = 0;
 		int diagnalrl = 0;
 		char player = getSymbol(p.getSymbol());
-		for(int i = 0; i < constSize; i++)
-		{
-			for(int j = 0; j < constSize; j++)
-			{
-				if (board[i][j] == player){ horizontal++; }
+		for (int i = 0; i < constSize; i++) {
+			for (int j = 0; j < constSize; j++) {
+				if (board[i][j] == player) { 
+					horizontal++; 
+				}
 				else { horizontal = 0; }
 				if (board[j][i] == player) vertical++;
 				else { vertical = 0; }
@@ -83,12 +77,10 @@ public class Board
 
 	public boolean checkValidMove(int position) {
 		for (int i = 1; i <= 9; i++) {
-
 			if (position == i) {
 				return true;
 			}
 		}
-
 		return false;
 	} 
 }
