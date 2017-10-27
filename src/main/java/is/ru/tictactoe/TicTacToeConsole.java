329777dda07f9ed730ac0db7a6f6c134edb6359d
kolliarnorsson <kolbeinna16@ru.is>
2017-10-27 14:21:43 +0000

    Refactoring last commit

--- src/main/java/is/ru/tictactoe/TicTacToeConsole.java
+++ src/main/java/is/ru/tictactoe/TicTacToeConsole.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class TicTacToeConsole {
   public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Player 1, what is your name?");
-    String player1Name = scanner.nextLine();
-    System.out.println("Player 2, what is your name?");
-    String player2Name = scanner.nextLine();
+    Scanner scanner = new Scanner(System.in);  
+    String player1Name = getName(1, scanner);
+    String player2Name = getName(2, scanner);
     Player player1 = new Player(player1Name, Symbol.CROSS);
     Player player2 = new Player(player2Name, Symbol.CIRCLE);
     TicTacToe game = new TicTacToe(player1, player2);
     scanner.close();
+  }
+
+  private static String getName(int number, Scanner scanner){
+    System.out.println("Player " + number + " what is your name?");
+    String name = scanner.nextLine();
+    return name;
   }
 }
