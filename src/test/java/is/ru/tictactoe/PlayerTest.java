3394668ea0425d362c8847179cfee3f5b8e21a5c
Fannarr <fannarr15@ru.is>
2017-10-27 15:47:33 +0000

    test

--- src/test/java/is/ru/tictactoe/PlayerTest.java
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -22,4 +22,22 @@ public class PlayerTest {
     player.incrementScore();
     assertEquals(1, player.getScore());
   }
+  @Test
+  public void testIncrementScore2(){
+    Player player1 = new Player("Steinar", Symbol.CROSS);
+    Player player2 = new Player("Kolbeinn Ari", Symbol.CIRCLE);
+    TicTacToe game = new TicTacToe(player1, player2); 
+    game.makeMove(1)
+    game.makeMove(2)
+    game.makeMove(4)
+    game.makeMove(3)
+    game.makeMove(7)
+    String winner = game.checkWinner();
+    if(winner == player1.getSymbol){
+      assertEquals(1, player1.getScore());
+    }
+    if(winner == player2.getSymbol){
+      assertEquals(1, player2.getScore());
+    }
+  }
 }
