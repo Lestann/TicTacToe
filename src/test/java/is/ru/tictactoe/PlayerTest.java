60cf262c6a5fe95ab5ea926e671b76337e9b0067
steinarh14 <steinarh14@ru.is>
2017-10-27 15:57:15 +0000

    fix test

--- src/test/java/is/ru/tictactoe/PlayerTest.java
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -26,18 +26,13 @@ public class PlayerTest {
   public void testIncrementScore2(){
     Player player1 = new Player("Steinar", Symbol.CROSS);
     Player player2 = new Player("Kolbeinn Ari", Symbol.CIRCLE);
-    TicTacToe game = new TicTacToe(player1, player2); 
-    game.makeMove(1)
-    game.makeMove(2)
-    game.makeMove(4)
-    game.makeMove(3)
-    game.makeMove(7)
+    TicTacToe game = new TicTacToe(player1, player2);
+    game.makeMove(1);
+    game.makeMove(2);
+    game.makeMove(4);
+    game.makeMove(3);
+    game.makeMove(7);
     String winner = game.checkWinner();
-    if(winner == player1.getSymbol){
-      assertEquals(1, player1.getScore());
-    }
-    if(winner == player2.getSymbol){
-      assertEquals(1, player2.getScore());
-    }
+    assertEquals(1, player1.getScore());
   }
 }
