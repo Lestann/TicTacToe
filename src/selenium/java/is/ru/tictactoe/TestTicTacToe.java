190af30ae8412c39abb4015d804e4ca42278c7ac
kolliarnorsson <kolbeinna16@ru.is>
2017-10-29 11:50:48 +0000

    added a little test

--- src/selenium/java/is/ru/tictactoe/TestTicTacToe.java
+++ src/selenium/java/is/ru/tictactoe/TestTicTacToe.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+
 
 public class TestTicTacToe extends SeleniumTestWrapper {
   @Test
@@ -15,11 +17,16 @@ public class TestTicTacToe extends SeleniumTestWrapper {
 
   @Test
   public void testSimpleAdd() throws Exception {
-
     Thread.sleep(2000);
-    WebElement input = driver.findElement(By.id("number"));
-    input.sendKeys("1");
-    Thread.sleep(2000);*/
+    WebElement td1 = driver.findElement(By.id("1"));
+    WebElement td2 = driver.findElement(By.id("2"));
+    WebElement td3 = driver.findElement(By.id("3"));
+    JavascriptExecutor executor = (JavascriptExecutor)driver;
+    executor.executeScript("arguments[0].click();", td1);
+    Thread.sleep(2000);
+    executor.executeScript("arguments[0].click();", td2);
+    Thread.sleep(2000);
+    executor.executeScript("arguments[0].click();", td3);
 
   }
 }
